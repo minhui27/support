ac564d36f3262c02857e5b01285a060ffe6fe0f4
minhui <minhui.phang@asahiarts.com>
2016-07-14 10:40:19 +0800

    fix error on session start

--- src/PhpSession.php
+++ src/PhpSession.php
@@ -26,7 +26,7 @@ class PhpSession
 
 	private function isStarted()
 	{
-		return session_status() === PHP_SESSION_ACTIVE;
+		return ((session_status() === PHP_SESSION_ACTIVE) || (session_status() === 2) || (session_status() === '2'));
 	}
 
 	public function get($key, $namespace = null)
